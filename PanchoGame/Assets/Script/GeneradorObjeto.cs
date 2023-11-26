182055b304f39fb77143a0ebcf4026b06013351c
kleber chavez <kleberenriqueos@gmail.com>
2023-11-26 12:00:18 -0500

    generacion de obstaculos

---
+++ PanchoGame/Assets/Script/GeneradorObjeto.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorObjeto : MonoBehaviour
+{
+
+    public float tiempoMax = 2;
+    private float tiempoInicial = 0;
+    public GameObject obstacle;
+
+    void Update()
+    {
+        if (tiempoInicial > tiempoMax)
+        {
+            GameObject obstaculoNuevo = Instantiate(obstacle);
+            obstaculoNuevo.transform.position = transform.position + new Vector3(0, 0, 0);
+            tiempoInicial = 0;
+        }
+        else
+        {
+            tiempoInicial += Time.deltaTime;
+        }
+    }
+}
